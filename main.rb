5c0753e0defff2e1d2e41741034f60f3aafd0dee
Ben Wyrosdick <ben.wyrosdick@gmail.com>
2009-05-23 16:44:36 -0500

    using isaac gem now and configure with scribe.yml

--- main.rb
+++ main.rb
@@ -5,21 +5,18 @@ require 'dm-validations'
 require 'dm-timestamps'
 
 DataMapper.setup(:default, (ENV["DATABASE_URL"] || "sqlite3:///#{Dir.pwd}/irclog.sqlite3"))
+config = YAML.load(open('scribe.yml').read)
 
 class LogEntry
   include DataMapper::Resource
   
   property :id, Integer, :serial => true  #primary serial key
   property :nick, Text, :nullable => false #cannot be null
-  property :user, Text, :nullable => false #cannot be null
-  property :host, Text, :nullable => false #cannot be null
-  property :recipient, Text, :nullable => false #cannot be null
+  property :userhost, Text, :nullable => false #cannot be null
+  property :channel, Text, :nullable => false #cannot be null
   property :message, Text, :nullable => false #cannot be null
   property :created_at, DateTime
   property :updated_at, DateTime
-  
-  # validates_present :nick, :user, :host, :recipient, :message
-  # validates_length :body, :minimum => 1
 end
 
 DataMapper.auto_upgrade!
@@ -29,16 +26,20 @@ get '/' do
   haml :index
 end
 
-# create
+# channel
+get '/channel/:channel' do
+  @messages = LogEntry.all(:channel => "##{params[:channel]}")
+  haml :channel
+end
 
+# create
 post '/log' do
   @log_entry = LogEntry.new(:nick => params[:nick],
-                            :user => params[:user],
-                            :host => params[:host],
-                            :recipient => params[:recipient],
+                            :userhost => params[:userhost],
+                            :channel => params[:channel],
                             :message => params[:message])
 
-  if (params[:secret] == 'ct_secret') and @log_entry.save
+  if (params[:secret] == config['secret']) and @log_entry.save
     "Saved log entry"
   else
     redirect "/"
@@ -46,7 +47,6 @@ post '/log' do
 end
 
 # show
-
 get "/:id" do
   @log_entry = LogEntry.get(params[:id])
   if @log_entry
